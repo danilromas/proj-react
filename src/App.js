490e50c753da15518e43a4e994e0d6b8f03283dd
danilromas <furiblack904@gmail.com>
2025-01-04 22:25:31 +0300

    modal with components

--- src/App.js
+++ src/App.js
@@ -11,19 +11,28 @@ function App() {
   const onClickMinus = () => {
     SetOpen(false)
   }
-  
+
+  const Modal = ({open, SetOpen, children}) => (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+        <div className="modal">
+          <svg onClick={onClickMinus} height="200" viewBox="0 0 200 200" width="200">    
+            <title />
+          </svg>
+          {children}   
+        </div>
+      </div>
+  )
+
+
+
   return (
     <div className="App">
       <button onClick={onClickPlus} className="open-modal-btn">✨ Открыть окно</button>
-      <div className={`overlay animated ${open ? 'show' : ''}`}>
-        <div className="modal">
-          <svg onClick={onClickMinus} height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
+        <Modal open={open} SetOpen={SetOpen}>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div>
+          <h3>жопа</h3>
+        </Modal>
+
     </div>
   );
 }
